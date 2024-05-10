ab236cf6d712044b5de5c549f3377c890934f87a
Jatin Kumar <155115799+jatin800@users.noreply.github.com>
2024-05-10 18:11:38 +0530

    1

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("lets get started");
+function containDuplicate(nums) {
+  let newArray = [];
+  for (let num of nums) {
+    if (newArray.includes(num)) {
+      return true;
+    }
+    newArray.push(num);
+  }
+}
+
+let nums = [1, 2, 3, 1];
+
+console.log(containDuplicate(nums));
+
+
+
+
+
+
