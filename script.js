90e3690f1940e0b66060506b279728a5faabd251
Jatin Kumar <155115799+jatin800@users.noreply.github.com>
2024-05-10 19:34:47 +0530

    2

--- script.js
+++ script.js
@@ -12,8 +12,17 @@ let nums = [1, 2, 3, 1];
 
 console.log(containDuplicate(nums));
 
+function validAnagram(string1, string2) {
+  let str1 = string1.split("").sort().join("");
+  let str2 = string2.split("").sort().join("");
 
+  if (str1 === str2) {
+    return true;
+  }
+  return false;
+}
 
+let s = "anagram";
+let t = "nagaram";
 
-
-
+console.log(validAnagram(s, t));
