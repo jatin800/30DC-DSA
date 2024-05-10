6864c894dc14b979de8b1a29761d78c6fc18706c
Jatin Kumar <155115799+jatin800@users.noreply.github.com>
2024-05-10 21:04:49 +0530

    3

--- script.js
+++ script.js
@@ -1,28 +1,44 @@
-function containDuplicate(nums) {
-  let newArray = [];
-  for (let num of nums) {
-    if (newArray.includes(num)) {
-      return true;
+// function containDuplicate(nums) {
+//   let newArray = [];
+//   for (let num of nums) {
+//     if (newArray.includes(num)) {
+//       return true;
+//     }
+//     newArray.push(num);
+//   }
+// }
+
+// let nums = [1, 2, 3, 1];
+
+// console.log(containDuplicate(nums));
+
+// function validAnagram(string1, string2) {
+//   let str1 = string1.split("").sort().join("");
+//   let str2 = string2.split("").sort().join("");
+
+//   if (str1 === str2) {
+//     return true;
+//   }
+//   return false;
+// }
+
+// let s = "anagram";
+// let t = "nagaram";
+
+// console.log(validAnagram(s, t));
+
+function twoSum(numberArr, target) {
+  for (let i = 0; i < numberArr.length; i++) {
+    for (let j = i + 1; j < numberArr.length; j++) {
+      if (numberArr[i] + numberArr[j] === target) {
+        return [i, j];
+      }
     }
-    newArray.push(num);
   }
 }
 
-let nums = [1, 2, 3, 1];
+let numberArr = [2, 7, 11, 15];
 
-console.log(containDuplicate(nums));
+let target = 9;
 
-function validAnagram(string1, string2) {
-  let str1 = string1.split("").sort().join("");
-  let str2 = string2.split("").sort().join("");
-
-  if (str1 === str2) {
-    return true;
-  }
-  return false;
-}
-
-let s = "anagram";
-let t = "nagaram";
-
-console.log(validAnagram(s, t));
+console.log(twoSum(numberArr, target));
