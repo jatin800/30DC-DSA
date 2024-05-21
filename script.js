2802edccd1922e2130a8f1289dab54737744fffa
Jatin Kumar <155115799+jatin800@users.noreply.github.com>
2024-05-22 03:41:47 +0530

    31

--- script.js
+++ script.js
@@ -841,21 +841,45 @@
 // const piles = [3, 6, 7, 11],
 //   h = 8;
 // console.log(func(piles, h));
-function func(nums) {
+// function func(nums) {
+//   let left = 0;
+//   right = nums.length - 1;
+//   if (nums[left] < nums[right]) {
+//     return nums[left];
+//   }
+//   while (left < right) {
+//     mid = Math.floor((left + right) / 2);
+//     if (nums[mid] > nums[right]) {
+//       left = mid + 1;
+//     } else {
+//       right = mid;
+//     }
+//   }
+//   return nums[left];
+// }
+// console.log(func(nums));
+var func = function (nums, target) {
   let left = 0;
-  right = nums.length - 1;
-  if (nums[left] < nums[right]) {
-    return nums[left];
-  }
-  while (left < right) {
-    mid = Math.floor((left + right) / 2);
-    if (nums[mid] > nums[right]) {
-      left = mid + 1;
+  let right = nums.length - 1;
+  while (left <= right) {
+    let mid = left + Math.floor((right - left) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    }
+    if (nums[left] <= nums[mid]) {
+      if (nums[left] <= target && target < nums[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
     } else {
-      right = mid;
+      if (nums[mid] < target && target <= nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
     }
   }
-  return nums[left];
-}
-console.log(func(nums));
-
+  return -1;
+};
+console.log(func(nums, target));
