f9b365f2e805f06e2fa5edab18a45195f002d569
Jatin Kumar <155115799+jatin800@users.noreply.github.com>
2024-05-23 03:19:03 +0530

    33

--- script.js
+++ script.js
@@ -868,35 +868,51 @@
 //   return -1;
 // };
 // console.log(func(nums, target));
-var TimeMap = function () {
-  this.map = new Map();
-};
-TimeMap.prototype.set = function (key, value, timestamp) {
-  if (!this.map.has(key)) {
-    this.map.set(key, []);
-  }
-  this.map.get(key).push({ value, timestamp });
-};
-TimeMap.prototype.get = function (key, timestamp) {
-  if (!this.map.has(key)) {
-    return "";
-  }
-  const pairs = this.map.get(key);
-  let left = 0;
-  let right = pairs.length - 1;
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
+// var TimeMap = function () {
+//   this.map = new Map();
+// };
+// TimeMap.prototype.set = function (key, value, timestamp) {
+//   if (!this.map.has(key)) {
+//     this.map.set(key, []);
+//   }
+//   this.map.get(key).push({ value, timestamp });
+// };
+// TimeMap.prototype.get = function (key, timestamp) {
+//   if (!this.map.has(key)) {
+//     return "";
+//   }
+//   const pairs = this.map.get(key);
+//   let left = 0;
+//   let right = pairs.length - 1;
+//   while (left <= right) {
+//     const mid = Math.floor((left + right) / 2);
 
-    if (pairs[mid].timestamp === timestamp) {
-      return pairs[mid].value;
-    } else if (pairs[mid].timeMap < timestamp) {
-      left = mid + 1;
-    } else {
-      right = mid - 1;
-    }
+//     if (pairs[mid].timestamp === timestamp) {
+//       return pairs[mid].value;
+//     } else if (pairs[mid].timeMap < timestamp) {
+//       left = mid + 1;
+//     } else {
+//       right = mid - 1;
+//     }
+//   }
+//   if (right === -1) {
+//     return "";
+//   }
+//   return pairs[right].value;
+// };
+
+function func(head) {
+  let previous = null;
+  let current = head;
+  while (current !== null) {
+    let next = current.next;
+    current.next = previous;
+    previous = current;
+    current = next;
   }
-  if (right === -1) {
-    return "";
-  }
-  return pairs[right].value;
-};
+  return previous;
+}
+
+const head = [1, 2, 3, 4, 5];
+
+console.log(func(head));
